0d370e0ad91a6997bc9abf7790e47205e8a666d0
DuduHenriqueMg <dudugama03@gmail.com>
2023-03-16 02:11:21 -0300

    commit1

--- Exercicio3/main.c
+++ Exercicio3/main.c
@@ -3,14 +3,12 @@
 
 int main(void) {
 
-    const float capacidade = 2.0, consumo = 0.3;
+    const double capacidade = 2.0, consumo = 0.3;
     float altura, largura, area, qtd_tinta, qtd_lata;
 
-    printf("===SAIBA A QUANTIDADE DE TINTA CERTA PRA PINTAR UMA PAREDE===\n");
-
-    printf("Qual a largura da parede (em metros)?");
+    printf("Digite a largura da parede (em metros): ");
     scanf("%f", &largura);
-    printf("Qual a altura da parede (em metros)? ");
+    printf("Digite a altura da parede (em metros): ");
     scanf("%f", &altura );
 
     area = largura*altura;
